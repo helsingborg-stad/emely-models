bc6ebf99a3cb8758cd9fdbd32ab08c5dca4b0001
bornhagel <alexander.hagelborn@nordaxon.com>
2021-05-26 11:04:40 +0200

    refactor: split dir api -> fika/interview

--- src/fika/api.py
+++ src/fika/api.py
@@ -18,13 +18,13 @@ app = FastAPI()
 logging.basicConfig(level=logging.NOTSET)
 
 # Opts for model loading
-model_path = Path(__file__).resolve().parents[2] / 'ParlAI/data/models/blender/blender_400Mdistill'
+model_path = Path(__file__).resolve().parents[2] / 'fika-model'
 opt_path = model_path / 'model.opt'
 opt = Opt.load(opt_path.as_posix())
 # opt['task'] = 'internal'
 opt['skip_generation'] = False
 opt['init_model'] = (model_path / 'model').as_posix()
-opt['no_cuda'] = True # Don't assume cuda on the gcp instance
+opt['no_cuda'] = True  # Don't assume cuda on the gcp instance
 # TODO: DO I need anything else here?
 
 #
@@ -37,13 +37,14 @@ async def startup_event():
     model = EmelyAgent(opt)
     return
 
+
 @app.post("/inference")
 async def inference(msg: ApiMessage):
-
     # Async model throughput
     reply = model.observe_and_act(msg.text)
 
     return ApiMessage(text=reply)
+
 
 # This endpoint can be used to both wake up the program and get the name of the model
 @app.get('/model-name')
