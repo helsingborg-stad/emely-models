f289f2b43cd0f15842de5da337951c2114930678
bornhagel <alexander.hagelborn@nordaxon.com>
2021-05-27 11:50:48 +0200

    feat: POST to /opt allows changing model opts like beam search parameters

--- src/fika/api.py
+++ src/fika/api.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
+from typing import Dict
+from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
-import asyncio
 from pathlib import Path
 
 from parlai.agents.emely.emely import EmelyAgent
@@ -11,6 +11,9 @@ import logging
 class ApiMessage(BaseModel):
     text: str
 
+class NewOpt(BaseModel):
+    new_opts: Dict
+
 
 app = FastAPI()
 
@@ -18,7 +21,7 @@ app = FastAPI()
 logging.basicConfig(level=logging.NOTSET)
 
 # Opts for model loading
-model_path = Path(__file__).resolve().parents[2] / 'models/fika-model'
+model_path = Path(__file__).resolve().parents[2] / 'models/blender_400Mdistill'
 opt_path = model_path / 'model.opt'
 opt = Opt.load(opt_path.as_posix())
 # opt['task'] = 'internal'
@@ -50,3 +53,43 @@ async def inference(msg: ApiMessage):
 def get_model():
     model_name = model_path.name
     return ApiMessage(text=model_name)
+
+
+@app.post("/opt")
+def change_opt(opts: NewOpt, response: Response):
+
+    opt_dict = opts.new_opts
+    global opt
+    param_changes = {}
+
+    for k, v in opt_dict.items():
+        try:
+            old_value = opt[k]
+            assert type(v) is type(old_value)
+            opt[k] = v
+            param_changes[k] = (old_value, v)
+        except KeyError as e:
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            return ApiMessage(text=f'Keyerror: {e}')
+        except AssertionError:
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            return ApiMessage(text=f'New value for {k} was {type(v)} but should be {type(old_value)}')
+
+    global model
+    try:
+        model = EmelyAgent(opt)
+        message = create_success_message(param_changes)
+        return ApiMessage(text=message)
+
+    except Exception as e:
+        response.status_code = status.HTTP_500
+        return ApiMessage(text=f'Error: {e}')
+    
+
+def create_success_message(param_changes):
+    text = 'Sucess!'
+    for k, v in param_changes.items():
+        text = text + f'\n{k}: {v[0]} -> {v[1]}'
+    return text
+        
+        
