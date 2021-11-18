ca4dbc0d128cbc3457b27323841d95ce19764a51
Marvin <info@nordaxon.com>
2021-11-18 10:18:29 +0100

    fix: changed project in batch scripts. I think they should be removed though

--- deploy-inference-testing.bat
+++ deploy-inference-testing.bat
@@ -1,3 +1,3 @@
 cp src/inference-testing/Dockerfile Dockerfile
-gcloud builds submit --tag eu.gcr.io/emelybrainapi/inference-testing --timeout 3000s && gcloud run deploy --image eu.gcr.io/emelybrainapi/inference-testing --platform managed --memory 2Gi --region europe-west1 --allow-unauthenticated --timeout 900s
+gcloud builds submit --tag eu.gcr.io/emely-gcp/inference-testing --timeout 3000s && gcloud run deploy --image eu.gcr.io/emely-gcp/inference-testing --platform managed --memory 2Gi --region europe-west1 --allow-unauthenticated --timeout 900s
 rm Dockerfile
--- deploy-interview.bat
+++ deploy-interview.bat
@@ -1,3 +1,3 @@
 cp src/interview/Dockerfile Dockerfile
-gcloud builds submit --tag eu.gcr.io/emelybrainapi/interview-model --timeout 3000s && gcloud run deploy --image eu.gcr.io/emelybrainapi/interview-model --platform managed --memory 4Gi --region europe-west3 --allow-unauthenticated --timeout 900s
+gcloud builds submit --tag eu.gcr.io/emely-gcp/interview-model --timeout 3000s && gcloud run deploy interview-model --image eu.gcr.io/emely-gcp/interview-model --platform managed --memory 4Gi --region europe-west3 --allow-unauthenticated --timeout 900s
 rm Dockerfile
