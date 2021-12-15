cc8b0aaf8bc97d972bd3df2c6442f44d002ebf33
bornhagel <alexander.hagelborn@nordaxon.com>
2021-12-15 15:17:24 +0100

    fix: added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="emely-models",
+    packages=find_packages(),
+    version="0.5.0",
+    description="Emely Models",
+    author="Alexander Hagelborn",
+)
